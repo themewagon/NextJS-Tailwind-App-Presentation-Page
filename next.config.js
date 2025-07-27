88f6685684275a3d3ecc2de110a74368e0f30026
utsaho <pipul.boddyo@gmail.com>
2025-07-27 15:00:08 +0600

    firstt commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/Notus-Next/' : '',
+  basePath: isProd ? '/Notus-Next' : '',
+  output: 'export'
+};
+
+module.exports = nextConfig;
